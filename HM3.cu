81897b21b88abcb6b4955b5b26cae47653c7544f
root <aiden05@126.com>
2025-10-21 14:46:09 +0800

    optimize

--- HM3.cu
+++ HM3.cu
@@ -1,10 +1,44 @@
 #include "testcase.h"
+#include <fstream>
+#include <cstring>
 
-int main(){
+int flag = 0;
+
+/*
+编译：
+nvcc HM3.cu -o [name] -lcublas -lcurand
+选项：
+（1）在其后加 -DCOLOR_STDOUT 选项，可以将输出颜色化显示。
+（2）在其后加 -DPYTEST_ON 选项，可以输出Pytorch的正确答案参考。
+
+运行：
+./[name] (f)
+选项：
+（1）省略“f”字符，则在控制台输出；
+（2）加上“f“字符，则输出到"testResult.txt"文件中。
+
+*/
+
+int main(int argc, char* argv[]){
+    std::streambuf* coutbuf = nullptr;
+    std::ofstream out;
+    if (argc == 2 && std::strcmp(argv[1], "f") == 0){
+        flag = 1;
+        out.open("testResult.txt");
+        coutbuf = std::cout.rdbuf(); //save old buf
+        std::cout.rdbuf(out.rdbuf());
+    }
+
     std::cout << BOLD << RED << "Running HM3 test cases..." << RESET << std::endl;
     testFC();
     testConv2d();
     testMaxpool2d();
     testSoftmaxAndCrossEntropy();
     std::cout << BOLD << GREEN << "All test cases passed!" << RESET << std::endl;
+
+    std::cout.flush();
+
+    if (flag == 1){
+        std::cout.rdbuf(coutbuf);
+    }
 }
--- pythontest.h
+++ pythontest.h
@@ -1,11 +1,26 @@
 #include <iostream>
 
-#define RESET   "\033[0m"
-#define RED     "\033[31m"
-#define GREEN   "\033[32m"
-#define YELLOW  "\033[33m"
-#define BLUE    "\033[34m"
-#define BOLD    "\033[1m"
+#ifdef COLOR_STDOUT
+#define BOLD   "\033[1m"
+#define RED    "\033[31m"
+#define GREEN  "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE   "\033[34m"
+#define RESET  "\033[0m"
+#else
+#define BOLD   ""
+#define RED    ""
+#define GREEN  ""
+#define YELLOW ""
+#define BLUE   ""
+#define RESET  ""
+#endif
+
+#ifdef PYTEST_ON
+#define PYTEST  ""
+#else
+#define PYTEST  return
+#endif
 
 std::string run_python(const std::string& cmd) {
     std::string result;
@@ -21,6 +36,7 @@ std::string run_python(const std::string& cmd) {
 
 
 void runPy(const char * root){
+    PYTEST;
     std::cout << BOLD << GREEN << "**Pytorch Result**" << RESET << std::endl;
     std::cout << GREEN << run_python(root) << RESET;
     std::cout << BOLD << GREEN << "**CORRECT**" << RESET << std::endl;
